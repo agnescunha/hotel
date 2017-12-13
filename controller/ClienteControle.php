25d54db21d183374f01b2a2eacb8dfeb5fe114a0
gabiSmachado <33360838+gabiSmachado@users.noreply.github.com>
2017-12-13 00:32:42 -0200

    Add files via upload

--- controller/ClienteControle.php
+++ controller/ClienteControle.php
@@ -1,140 +1,47 @@
 <?php
-	require_once '../../model/Banco.php';
-	require_once '../../model/Cliente.php';
+	require_once '../../persistencia/ClientePersistencia.php';
 
 	class ClienteControle{
-		private $banco;
-	    private $conn;
+		private $cliePers;
 
+		public function ClienteControle(){
+			$this->cliePers = new ClientePersistencia();
+		}
 
 		public function selectAll(){
-			$banco = new Banco();
-			$banco->abrirConexao();
-			$conn = $banco->getConexao();
-			$sql = "select*from cliente";
-			$result = mysqli_query($conn,$sql);
-			$clientes=array();
-			if (mysqli_num_rows($result) > 0) {
-   				 while($linha = mysqli_fetch_assoc($result)) {
-					$cliente = new Cliente($linha['nome'],$linha['rg'],$linha['cpf'],$linha['endereco'],$linha['data_nascimento'],$linha['telefone1'],
-					$linha['telefone2'],$linha['email'],$linha['senha']);
-				 $cliente->setId($linha['id']);
-				 array_push($clientes, $cliente);
-				}	
-			}else{
-
-			}
-			return $clientes;
+			return $this->cliePers->selectAll();
 		}
 
 		public function selectFromId($id){
-			$banco = new Banco();
-			$banco->abrirConexao();
-			$conn = $banco->getConexao();
-			$sql = "select*from cliente where id = '$id'";
-			$result = mysqli_query($conn,$sql);
-			$cliente= null;
-			if (mysqli_num_rows($result) > 0) {
-   				$linha = mysqli_fetch_assoc($result);
-					$cliente = new Cliente($linha['nome'],$linha['rg'],$linha['cpf'],$linha['endereco'],$linha['data_nascimento'],$linha['telefone1'],
-					$linha['telefone2'],$linha['email'],$linha['senha']);
-				 	$cliente->setId($linha['id']);		
-			}else{
-
-			}
-			return $cliente;
+			return $this->cliePers->selectFromId($id);
 		}
 
 		public function update($cliente){
-			$banco = new Banco();
-			$banco->abrirConexao();
-			$conn = $banco->getConexao();
-			$id = $cliente->getId();
-			$nome = $cliente->getNome();
-			$cpf = $cliente->getCpf();
-			$rg = $cliente->getRg();
- 			$telefone1 = $cliente->getTelefone1();
-			$telefone2 = $cliente->getTelefone2();
-			$aniversario = $cliente->getAniversario();
-			$email = $cliente->getEmail();
-			$senha = $cliente->getSenha();
-			$endereco = $cliente->getEndereco();
-			$sql = "update cliente set nome='$nome', rg='$rg', cpf='$cpf', endereco='$endereco', data_nascimento='$aniversario',
-			telefone1='$telefone1', telefone2='$telefone2',email='$email',senha='$senha' where id = '$id'";
-			mysqli_query($conn, $sql);
+			$this->cliePers->update($cliente);
 		}
 
 		public function insert($cliente){
-			$banco = new Banco();
-			$banco->abrirConexao();
-			$conn = $banco->getConexao();
-			$nome = $cliente->getNome();
-			$cpf = $cliente->getCpf();
-			$rg = $cliente->getRg();
- 			$telefone1 = $cliente->getTelefone1();
-			$telefone2 = $cliente->getTelefone2();
-			$aniversario = $cliente->getAniversario();
-			$email = $cliente->getEmail();
-			$senha = $cliente->getSenha();
-			$endereco = $cliente->getEndereco();
-			$sql = "insert into `cliente`( `nome`, `rg`, `cpf`, `endereco`, `data_nascimento`, `telefone1`, `telefone2`, `email`, `senha`) values 
-						('$nome', '$rg', '$cpf','$endereco', '$aniversario','$telefone1', '$telefone2','$email','$senha')";
-			mysqli_query($conn, $sql);
+			$this->cliePers->insert($cliente);
 		}
 
 		public function delete($id){
-			$banco = new Banco();
-			$banco->abrirConexao();
-			$conn = $banco->getConexao();
-			$sql = "delete from cliente where id = '$id'";
-			mysqli_query($conn, $sql);
+			$this->cliePers->delete($id);
 		}
 
 		public function selectFromName($nome){
-			$banco = new Banco();
-			$banco->abrirConexao();
-			$conn = $banco->getConexao();
-			$sql = "select*from cliente where nome like '%$nome%'";
-			$result = mysqli_query($conn,$sql);
-			$clientes=array();
-			if (mysqli_num_rows($result) > 0) {
-   				while($linha = mysqli_fetch_assoc($result)) {
-					$cliente = new Cliente($linha['nome'],$linha['rg'],$linha['cpf'],$linha['endereco'],$linha['data_nascimento'],$linha['telefone1'],
-					$linha['telefone2'],$linha['email'],$linha['senha']);
-				 $cliente->setId($linha['id']);
-				 array_push($clientes, $cliente);
-				}		
-			}else{
-
-			}
-			return $clientes;
+			return $this->cliePers->selectFromName($nome);
 		}
 
 		public function verificarRG($rg){
-			$banco = new Banco();
-			$banco->abrirConexao();
-			$conn = $banco->getConexao();
-			$sql = "select*from cliente where rg = '$rg'";
-			$result = mysqli_query($conn,$sql);
-			return mysqli_num_rows($result);
+			return $this->cliePers->verificarRG($rg);
 		}
 
 		public function verificarCPF($cpf){
-			$banco = new Banco();
-			$banco->abrirConexao();
-			$conn = $banco->getConexao();
-			$sql = "select*from cliente where cpf = '$cpf'";
-			$result = mysqli_query($conn,$sql);
-			return mysqli_num_rows($result);
+			return $this->cliePers->verificarCPF($cpf);
 		}
 
 		public function verificarEmail($email){
-			$banco = new Banco();
-			$banco->abrirConexao();
-			$conn = $banco->getConexao();
-			$sql = "select*from cliente where email = '$email'";
-			$result = mysqli_query($conn,$sql);
-			return mysqli_num_rows($result);
+			return $this->cliePers->verificarEmail($email);
 		}
 	}
 ?>
